3f38eda31b1233c96d9d1729f5d26c010b67aa08
julie-devos <julie.devos@epitech.eu>
2020-12-07 15:05:47 +0100

    commit

---
+++ exo_4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+		<title>PhpPart1Exo4</title>
+</head>
+<body>
+	<?php
+	$name = "Julie";
+	$age = 27;
+	$moyenne = 15.2;
+	$info => "true";
+	$rien => "null";
+	?>
+	<p><?=$name $age $moyenne $info $rien?></p>
+
+</body>
+</html>
